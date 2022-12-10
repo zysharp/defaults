6a2d2c5d9946ad83bfb7d343d8077066e8473d65
Florian Bernd <git@flobernd.de>
2022-12-10 15:49:11 +0100

    Add Visual Studio solution file (#2)

---
+++ project/ZySharp.Template.Tests/Test.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ZySharp.Template.Tests;
+
+[Trait("Category", "Unit")]
+public sealed class Test
+{
+    [Fact]
+    public void TestFact()
+    {
+        Assert.True(true);
+    }
+}
