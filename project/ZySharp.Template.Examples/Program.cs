890b012f7d495422654b925383c03a503073c234
Florian Bernd <git@flobernd.de>
2022-12-10 15:35:50 +0100

    Add Visual Studio solution file

---
+++ project/ZySharp.Template.Examples/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZySharp.Template.Examples;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        throw new NotImplementedException();
+    }
+}
